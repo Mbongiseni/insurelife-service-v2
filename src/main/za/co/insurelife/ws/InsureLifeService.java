f66ea2b2f09273f6e459929257a2bbea4ea393a0
Mbongiseni <mbongiseni.ngubane@fnb.co.za>
2020-01-11 19:34:08 +0200

    Project configurations.

--- src/main/za/co/insurelife/ws/InsureLifeService.java
+++ src/main/za/co/insurelife/ws/InsureLifeService.java
@@ -2,6 +2,11 @@ package za.co.insurelife.ws;
 
 import javax.jws.WebService;
 
+/*
+ *
+ * Marks Java class as an implementation of Webservice or Java interface as a webservice interface
+ */
 @WebService(name="InsureLifeService")
 public interface InsureLifeService {
+    GetAllClientsResponse
 }
